a5cbd8bbf4d1a0f3df621bafab05a8012a72b2bf
George Nance <georgenancejr@gmail.com>
2024-02-01 23:18:39 -0700

    Update styling in [slug].vue and tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,8 +12,23 @@ module.exports = {
     extend: {
       boxShadow: {
         zoop: "rgba(102, 109, 128, 0.08) 0px 1.2672px 1.2672px 0px, rgba(102, 109, 128, 0.08) 0px 5.06879px 10.1376px 0px",
-        zoopdark:
-          "rgba(10, 10, 10, 0.2) 0px 1.2672px 1.2672px 0px, rgba(10, 10, 10, 0.2) 0px 5.06879px 10.1376px 0px",
+        zoopdark: "rgba(10, 10, 10, 0.2) 0px 1.2672px 1.2672px 0px, rgba(10, 10, 10, 0.2) 0px 5.06879px 10.1376px 0px",
+      },
+      typography:{
+        DEFAULT: {
+          css: {
+            // code:{
+            //   'background-color': 'red !important',
+            // },
+              'code::before': {
+                content: '""',
+              },
+              'code::after': {
+                content: '""',
+              }
+          },
+        },
+
       },
     },
 
