23a49ffa167fce8c3bf3e6022de1aeee149f1431
George Nance <georgenancejr@gmail.com>
2024-02-03 20:09:46 -0700

    Refactor UsesHeader.vue: Remove unnecessary background color class

--- components/App/UsesHeader.vue
+++ components/App/UsesHeader.vue
@@ -4,7 +4,7 @@
       class="relative after:-z-10 after:block after:h-[2px] after:absolute after:top-1/2 after:transform after:w-full after:left-0 after:right-0"
     >
       <span
-        class="font-medium text-sm text-gray-600 dark:text-gray-500 bg-gray-50 dark:bg-black pr-4"
+        class="font-medium text-sm text-gray-600 dark:text-gray-500  pr-4"
         >{{ title }}</span>
         <hr class="w-full " />
         <!-- Line -->
