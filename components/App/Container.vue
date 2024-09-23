0b197890956ab76cb1369f175e098cf8335ebf5b
George Nance <georgenancejr@gmail.com>
2024-09-23 08:11:50 -0700

    Refactor container and footer components

--- components/App/Container.vue
+++ components/App/Container.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container ">
-    <slot />
-  </div>
+	<div class="container ">
+		<slot />
+	</div>
 </template>
 
 <script lang="ts" setup>
@@ -10,7 +10,7 @@
 
 <style>
 .container {
-  /* Add Tailwind CSS classes or custom styles here */
-  @apply max-w-6xl mx-auto px-4 sm:px-6 lg:px-8;
+	/* Add Tailwind CSS classes or custom styles here */
+	@apply max-w-6xl mx-auto px-4 sm:px-6 lg:px-8;
 }
 </style>
