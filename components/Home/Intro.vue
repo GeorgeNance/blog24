38bad28cd435c3741e0cb7ecf49e013d5d3254d3
George Nance <hello@georgenance.com>
2025-02-22 15:57:36 -0700

    Update profile image, intro styling, and code block component

--- components/Home/Intro.vue
+++ components/Home/Intro.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="space-y-6 flex content-center flex-col">
-    <NuxtImg ref="avatar" src="/avatar.jpg" alt="George Nance" draggable="false"
-             class="ring-2 border mx-auto ring-gray-200 border-gray-300 dark:ring-primary/10 dark:border-gray-800 hover:ring-4 transition-all duration-300 bg-gray-200 dark:bg-gray-900 rounded-full h-32 w-32 md:h-32 md:w-32 "
-             sizes="128px" placeholder format="webp" />
+    <NuxtImg ref="avatar" src="/img/profile.jpg" alt="George Nance" draggable="false"
+             class="ring-2 border mx-auto ring-gray-200 border-gray-300 dark:ring-primary/10 dark:border-gray-800 hover:ring-4 hover:scale-105 hover:rotate-3 hover:shadow-lg transition-all duration-300 ease-in-out transform bg-gray-200 dark:bg-gray-900 rounded-full h-32 w-32 md:h-48 md:w-48 "
+             sizes="(min-width: 768px) 192px, 128px" placeholder format="webp" />
 
     <h1 class="text-xl font-bold tracking-tight text-gray-800 dark:text-gray-100 text-center">
       Welcome!👋
     </h1>
     <p ref="description" class="text-gray-900 dark:text-gray-300">
-      I'm <span class="text-cyan-500 font-bold">George Nance</span>, I'm a full stack web developer based in Phoenix, AZ.
+      I'm <span class="text-cyan-500 font-bold">George Nance</span>, I'm a full stack web developer based in Phoenix,
+      AZ.
       I am
       passionate about technology and all
       things nerdy. Sometimes I write and publish it here.
