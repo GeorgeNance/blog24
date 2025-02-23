9de37b43e79e9d452c6d7a4d623d369caddfea98
George Nance <hello@georgenance.com>
2025-02-23 08:59:48 -0700

    Temporarily disable theme toggle component

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -49,7 +49,7 @@
                 </div>
               </div>
             </template>
-            <AppThemeToggle />
+            <!-- <AppThemeToggle /> -->
           </div>
         </div>
       </div>
