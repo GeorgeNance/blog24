d2bc732b69ffb48da0d96ffa7fb37af7760bb8cd
George Nance <hello@georgenance.com>
2025-02-24 19:44:19 -0700

    Change Tailwind dark mode to system preference

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  darkMode: 'class',
+  darkMode: 'media',
   content: [
     "./components/**/*.{js,vue,ts}",
     "./layouts/**/*.vue",
